299c631d2a2125bb0d9022aa18729bb261a041ac
Quang Nguyen <qnguyen3411@gmail.com>
2018-10-05 23:30:42 -0700

    second commit

--- public/src/app/http.service.ts
+++ public/src/app/http.service.ts
@@ -6,58 +6,70 @@ import { HttpClient } from '@angular/common/http';
 })
 export class HttpService {
 
-  constructor(private _http: HttpClient){
-    this.getPokemon();
+  constructor(private _http: HttpClient) {
+    this.getPokemon()
+    .then(data => this.getPokemonAbilities(data))
+    .then(data => this.getPokemonsThatShareAbilities(...data))
+    .then(data => this.getOtherPokeAbilities(...data))
+    .catch(err => {
+      console.log(err)
+    })
   }
 
   getPokemon() {
-    let pokemon = this._http.get('https://pokeapi.co/api/v2/pokemon/1/');
-    pokemon.subscribe(pokemon => {
-      const ownAbilities = pokemon["abilities"];
-      const name = pokemon["name"];
-      let abilArr: Array<string> = [];
-
-      for (let ownAbil of pokemon["abilities"]) {
-        abilArr.push(ownAbil["ability"]["name"]);
-      }
-      console.log(`${name} knows ${abilArr.join(' and ')}.`)
-      for (let ownAbil of ownAbilities) {        
-        // Get ability data
-        const url = ownAbil["ability"]["url"]
-        let ability = this._http.get(url)
-        ability.subscribe(abilityData => {
-          let count = 0;
-          for(let i in abilityData["pokemon"]) {
-            count++;
-          }
-          console.log(`${count} other Pokemons shares the ability ${ownAbil["ability"]["name"]} with ${name}`)
-
-          for(let i in abilityData["pokemon"]) {
-
-            const url = abilityData["pokemon"][i]["pokemon"]["url"];
-            const otherPokemon = this._http.get(url);
-            otherPokemon.subscribe(otherPokeData => {
-              const otherPokeName = otherPokeData["name"];
-              let otherAbilArr: Array<string> = []
-              for (let otherAbil of otherPokeData["abilities"]) {
-                let dupe: Boolean;
-                for (let ownAbil of ownAbilities) {
-                  if (otherAbil["ability"]["name"] == ownAbil["ability"]["name"]) {
-                    dupe = true;
-                  }
-                }
-                if (!dupe) {
-                otherAbilArr.push(otherAbil["ability"]["name"])
-                }
-              }
-              if (otherAbilArr.length) {
-              console.log(`${otherPokeName} also knows ${otherAbilArr.join(' and ')}.`)
-              }
-            })
-          }
-
-        })
-      }
-    })
+    return this._http.get('https://pokeapi.co/api/v2/pokemon/1/').toPromise();
   }
+
+  getPokemonAbilities(thisPokeData: Object) {
+    const name = thisPokeData["name"];
+    let abilData = thisPokeData["abilities"] as Array<Object>
+    let abilNameArr: Array<string> = [];
+    let abilUrlArr: Array<string> = [];
+    abilData.forEach(abil => {
+      abilNameArr.push(abil["ability"]["name"]);
+      abilUrlArr.push(abil["ability"]["url"]);
+    })
+    console.log(`${name}'s abilities are ${abilNameArr.join(' and ')}.`)
+    const getAbilitiesDataPromise = Promise.all(
+      abilUrlArr.map(url => this._http.get(url).toPromise()))
+    return Promise.all([
+      Promise.resolve(thisPokeData), getAbilitiesDataPromise
+    ]);
+  }
+
+  getPokemonsThatShareAbilities(thisPokeData: Object, abilitiesData: Array<Object>) {
+    let otherPokemons = abilitiesData.map(ability =>
+      ability["pokemon"] as Array<Object>
+    )
+    for (let i in abilitiesData) {
+      console.log(
+        `${otherPokemons[i].length} other Pokemons share the ability 
+        ${abilitiesData[i]["name"]} with this Pokeman ${thisPokeData["name"]}`)
+    }
+    const getOtherPokeData = []
+      .concat(...otherPokemons)
+      .map(pokemon => pokemon["pokemon"]["url"])
+      .map(url => this._http.get(url).toPromise())
+    const getOtherPokeDataPromise = Promise.all(getOtherPokeData)
+    return Promise.all([Promise.resolve(thisPokeData), getOtherPokeDataPromise])
+  }
+
+  getOtherPokeAbilities(thisPokeData: Object, otherPokeData: Array<Object>) {
+    const thisPokeAbility = (thisPokeData["abilities"] as Array<Object>)
+      .map(abil => abil["ability"]["name"]);
+    const otherPokeAbility = otherPokeData.map(otherPoke => {
+      return (otherPoke["abilities"] as Array<Object>)
+        .map(abil => abil["ability"]["name"])
+        .filter(abilName => !thisPokeAbility.includes(abilName))
+    })
+    for (let i in otherPokeData) {
+      if (otherPokeAbility[i].length) {
+        const name = otherPokeData[i]["name"];
+        const abilities = otherPokeAbility[i].join(" and ");
+        console.log(`${name} also has ${abilities}.`)
+      }
+    }
+    return;
+  }
+
 }
